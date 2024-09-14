6950acf89567995e5d64973906b6f1f0c6454cae
Thiraiyan Mooneesawmy <Niseiyen@gmail.com>
2024-09-14 16:26:26 -0400

    Suppresion des fichiers inutilisés

--- src/main/java/com/lacouf/rsbjwt/ReactSpringSecurityJwtApplication.java
+++ src/main/java/com/lacouf/rsbjwt/ReactSpringSecurityJwtApplication.java
@@ -1,9 +1,6 @@
 package com.lacouf.rsbjwt;
 
 import com.lacouf.rsbjwt.model.*;
-import com.lacouf.rsbjwt.repository.EmprunteurRepository;
-import com.lacouf.rsbjwt.repository.GestionnaireRepository;
-import com.lacouf.rsbjwt.repository.PreposeRepository;
 import com.lacouf.rsbjwt.repository.UserAppRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,18 +12,11 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class ReactSpringSecurityJwtApplication implements CommandLineRunner {
-
-    private final GestionnaireRepository gestionnaireRepository;
-    private final EmprunteurRepository emprunteurRepository;
-    private final PreposeRepository preposeRepository;
     private final UserAppRepository userAppRepository;
 
     private final PasswordEncoder passwordEncoder;
 
-    public ReactSpringSecurityJwtApplication(GestionnaireRepository gestionnaireRepository, EmprunteurRepository emprunteurRepository, PreposeRepository preposeRepository, UserAppRepository userAppRepository, PasswordEncoder passwordEncoder) {
-        this.gestionnaireRepository = gestionnaireRepository;
-        this.emprunteurRepository = emprunteurRepository;
-        this.preposeRepository = preposeRepository;
+    public ReactSpringSecurityJwtApplication(UserAppRepository userAppRepository, PasswordEncoder passwordEncoder) {
         this.userAppRepository = userAppRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -37,36 +27,6 @@ public class ReactSpringSecurityJwtApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        gestionnaireRepository.save(
-                Gestionnaire.builder()
-                        .firstName("Gerard")
-                        .lastName("Biblio")
-                        .email("l@l.com")
-                        .password(passwordEncoder.encode("bib"))
-                        .matricule("0000001")
-                        .phoneNumber("123-456-7890")
-                        .build()
-        );
-        emprunteurRepository.save(
-                Emprunteur.builder()
-                        .firstName("Isidor")
-                        .lastName("Teurteur")
-                        .email("ll@l.com")
-                        .password(passwordEncoder.encode("bib"))
-                        .since(LocalDate.of(2020, 10,20))
-                        .build()
-        );
-        preposeRepository.save(
-                Prepose.builder()
-                        .firstName("Chandeuse")
-                        .lastName("Lixor")
-                        .email("lll@l.com")
-                        .password(passwordEncoder.encode("bib"))
-                        .passeKey("12345")
-                        .build()
-        );
-        final Optional<UserApp> userAppByEmail = userAppRepository.findUserAppByEmail("l@l.com");
-        userAppByEmail.ifPresent(userApp -> System.out.println("user " + userAppByEmail));
 
     }
 }
--- src/main/java/com/lacouf/rsbjwt/service/UserAppService.java
+++ src/main/java/com/lacouf/rsbjwt/service/UserAppService.java
@@ -1,9 +1,6 @@
 package com.lacouf.rsbjwt.service;
 
 import com.lacouf.rsbjwt.model.*;
-import com.lacouf.rsbjwt.repository.EmprunteurRepository;
-import com.lacouf.rsbjwt.repository.GestionnaireRepository;
-import com.lacouf.rsbjwt.repository.PreposeRepository;
 import com.lacouf.rsbjwt.repository.UserAppRepository;
 import com.lacouf.rsbjwt.service.dto.*;
 import com.lacouf.rsbjwt.security.JwtTokenProvider;
@@ -22,9 +19,6 @@ public class UserAppService {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserAppRepository userAppRepository;
-    private final EmprunteurRepository emprunteurRepository;
-    private final PreposeRepository preposeRepository;
-    private final GestionnaireRepository gestionnaireRepository;
 
     public String authenticateUser(LoginDTO loginDto) {
         Authentication authentication = authenticationManager.authenticate(
@@ -32,35 +26,14 @@ public class UserAppService {
         return jwtTokenProvider.generateToken(authentication);
     }
 
-    public UserDTO getMe(String token) {
-        token = token.startsWith("Bearer") ? token.substring(7) : token;
-        String email = jwtTokenProvider.getEmailFromJWT(token);
-        UserApp user = userAppRepository.findUserAppByEmail(email).orElseThrow(UserNotFoundException::new);
-        return switch(user.getRole()){
-            case EMPRUNTEUR -> getEmprunteurDto(user.getId());
-            case PREPOSE -> getPreposeDto(user.getId());
-            case GESTIONNAIRE -> getGestionnaireDto(user.getId());
-        };
-    }
-
-    private GestionnaireDto getGestionnaireDto(Long id) {
-        final Optional<Gestionnaire> gestionnaireOptional = gestionnaireRepository.findById(id);
-        return gestionnaireOptional.isPresent() ?
-                GestionnaireDto.create(gestionnaireOptional.get()) :
-                GestionnaireDto.empty();
-    }
-
-    private PreposeDto getPreposeDto(Long id) {
-        final Optional<Prepose> preposeOptional = preposeRepository.findById(id);
-        return preposeOptional.isPresent() ?
-                PreposeDto.create(preposeOptional.get()) :
-                PreposeDto.empty();
-    }
-
-    private EmprunteurDto getEmprunteurDto(Long id) {
-        final Optional<Emprunteur> emprunteurOptional = emprunteurRepository.findById(id);
-        return emprunteurOptional.isPresent() ?
-                EmprunteurDto.create(emprunteurOptional.get()) :
-                EmprunteurDto.empty();
-    }
+//    public UserDTO getMe(String token) {
+//        token = token.startsWith("Bearer") ? token.substring(7) : token;
+//        String email = jwtTokenProvider.getEmailFromJWT(token);
+//        UserApp user = userAppRepository.findUserAppByEmail(email).orElseThrow(UserNotFoundException::new);
+//        return switch(user.getRole()){
+//            case EMPRUNTEUR -> getEmprunteurDto(user.getId());
+//            case PREPOSE -> getPreposeDto(user.getId());
+//            case GESTIONNAIRE -> getGestionnaireDto(user.getId());
+//        };
+//    }
 }

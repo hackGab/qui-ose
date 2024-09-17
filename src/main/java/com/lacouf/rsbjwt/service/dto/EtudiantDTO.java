8cbed5bbfeb244f29d804e3c97c643162ca6c38f
Thiraiyan Mooneesawmy <Niseiyen@gmail.com>
2024-09-17 10:19:27 -0400

    Creation de la classe EtudiantDTO et modification de la classe UserDTO

--- src/main/java/com/lacouf/rsbjwt/service/dto/EtudiantDTO.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/EtudiantDTO.java
@@ -1,18 +1,17 @@
 package com.lacouf.rsbjwt.service.dto;
 
-import com.lacouf.rsbjwt.model.UserApp;
+import com.lacouf.rsbjwt.model.Etudiant;
 import com.lacouf.rsbjwt.model.auth.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserDTO {
+public class EtudiantDTO {
     private Long id;
     private String firstName;
     private String lastName;
@@ -20,19 +19,19 @@ public class UserDTO {
     private String phoneNumber;
     private Role role;
 
-    public UserDTO(String firstName, String lastName, Role role) {
+
+    public EtudiantDTO(String firstName, String lastName, Role role) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.role = role;
     }
 
-    public UserDTO(UserApp user) {
-        this.id = user.getId();
-        this.firstName = user.getFirstName();
-        this.lastName = user.getLastName();
-        this.phoneNumber = user.getPhoneNumber();
-        this.email = user.getEmail();
-        this.role = user.getRole();
+    public EtudiantDTO(Etudiant etudiant) {
+        this.id = etudiant.getId();
+        this.firstName = etudiant.getFirstName();
+        this.lastName = etudiant.getLastName();
+        this.email = etudiant.getEmail();
+        this.phoneNumber = etudiant.getPhoneNumber();
+        this.role = etudiant.getRole();
     }
-
 }

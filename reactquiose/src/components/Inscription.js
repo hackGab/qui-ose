2f108889b1659ff3d0d5140f3779154c084c3fef
Gabriel Laplante <2215033@claurendeau.qc.ca>
2024-09-18 11:36:24 -0400

    Modification de l'animation et du positionnement de balise

--- reactquiose/src/components/Inscription.js
+++ reactquiose/src/components/Inscription.js
@@ -6,57 +6,62 @@ function Inscription() {
         <form>
             <legend>Champs obligatoires*</legend>
 
-            <div className="form-group">
-                <label htmlFor="prenom">Prénom*</label>
-                <input type="text" className="form-control" id="prenom" name="prenom" placeholder="John"
-                           pattern={"[A-Za-z]+"} autoFocus={true} required/>
+            <div className='row'>
+                <div className='col-lg-12 col-md-4 col-4 m-auto'>
+                    <div className="form-group">
+                        <label htmlFor="prenom">Prénom*</label>
+                        <input type="text" className="form-control" id="prenom" name="prenom" placeholder="John"
+                                   pattern={"[A-Za-z]+"} autoFocus={true} required/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="nom">Nom*</label>
+                        <input type="text" className="form-control" id="nom" name="nom" placeholder="Doe"
+                               pattern={"[A-Za-z]+"} required/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="mpd">Mot de passe*</label>
+                        <input type="password" className="form-control" id="mpd" name="mpd" placeholder="********"
+                               required/>
+                    </div>
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="nom">Nom*</label>
-                    <input type="text" className="form-control" id="nom" name="nom" placeholder="Doe"
-                           pattern={"[A-Za-z]+"} required/>
+                <div className='col-lg-12 col-md-6 col-6 m-auto'>
+
+                    <div className="form-group">
+                        <label htmlFor="mpdConfirm">Confirmation du mot de passe*</label>
+                        <input type="password" className="form-control" id="mpdConfirm" name="mpdConfirm"
+                               placeholder="********" required/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email">Email*</label>
+                        <input type="email" className="form-control" id="email" name="email"
+                               placeholder="johndoe@gmail.com" required/>
+                    </div>
+
+
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="num">Numéro de téléphone</label>
+                        <InputMask
+                            className="form-control"
+                            mask="(999)-999-9999"
+                            maskChar={null}
+                            id="num"
+                            placeholder="(514)-123-4567"
+                            name="num">
+                            {(inputProps) => <input type="tel" {...inputProps} />}
+                        </InputMask>
+                    </div>
                 </div>
-
-                <div className="form-group">
-                    <label htmlFor="mpd">Mot de passe*</label>
-                    <input type="password" className="form-control" id="mpd" name="mpd" placeholder="********"
-                           required/>
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="mpdConfirm">Confirmation du mot de passe*</label>
-                    <input type="password" className="form-control" id="mpdConfirm" name="mpdConfirm"
-                           placeholder="********" required/>
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="email">Email*</label>
-                    <input type="email" className="form-control" id="email" name="email"
-                           placeholder="johndoe@gmail.com" required/>
-                </div>
+            </div>
 
 
-                <div className="form-group">
-                    <label className="form-label" htmlFor="num">Numéro de téléphone</label>
-                    <InputMask
-                        className="form-control"
-                        mask="(999)-999-9999"
-                        maskChar={null}
-                        id="num"
-                        placeholder="(514)-123-4567"
-                        name="num">
-                        {(inputProps) => <input type="tel" {...inputProps} />}
-                    </InputMask>
-                </div>
+            <button className="btn btn-primary w-50" type="submit">S'inscrire</button>
 
 
-                <button className="btn btn-primary w-50" type="submit">S'inscrire</button>
-
-                <br/>
-
-
-                <small>Déjà un compte? <a href="/login">Connectez-vous</a></small>
+            <small>Déjà un compte? <a href="/login">Connectez-vous</a></small>
         </form>
     )
 }

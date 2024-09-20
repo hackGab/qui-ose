a6ab86782db1921e659cb18266ce908fda64dfae
Thiraiyan Mooneesawmy <Niseiyen@gmail.com>
2024-09-20 13:03:28 -0400

    Service et controller pour la connexion d'un utilisateur

--- src/main/java/com/lacouf/rsbjwt/presentation/UserController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/UserController.java
@@ -20,18 +20,18 @@ public class UserController {
 
 	private final UserAppService userService;
 
-//	@PostMapping("/login")
-//	public ResponseEntity<JWTAuthResponse> authenticateUser(@RequestBody LoginDTO loginDto){
-//		try {
-//			String accessToken = userService.authenticateUser(loginDto);
-//			final JWTAuthResponse authResponse = new JWTAuthResponse(accessToken);
-//			return ResponseEntity.accepted()
-//					.contentType(MediaType.APPLICATION_JSON)
-//					.body(authResponse);
-//		} catch (Exception e) {
-//			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new JWTAuthResponse());
-//		}
-//	}
+	@PostMapping("/login")
+	public ResponseEntity<JWTAuthResponse> authenticateUser(@RequestBody LoginDTO loginDto){
+		try {
+			String accessToken = userService.authenticateUser(loginDto);
+			final JWTAuthResponse authResponse = new JWTAuthResponse(accessToken);
+			return ResponseEntity.accepted()
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(authResponse);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new JWTAuthResponse());
+		}
+	}
 
 //	@GetMapping("/me")
 //	public ResponseEntity<UserDTO> getMe(HttpServletRequest request){

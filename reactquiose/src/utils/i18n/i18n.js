8c5a550e60a14ceec3de4968f61a2e9ab4b46bcd
Loky115 <lokeshwarbasdeo23@hotmail.com>
2024-09-25 20:29:52 -0400

    translation selon le language du moteur de recherche

--- reactquiose/src/utils/i18n/i18n.js
+++ reactquiose/src/utils/i18n/i18n.js
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enLang  from './locales/en/en.json';
 import frLang  from './locales/fr/fr.json';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
     en: {
@@ -13,16 +15,18 @@ const resources = {
 };
 
 i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(LanguageDetector)
+    .use(initReactI18next)// passes i18n down to react-i18next
+    .use(Backend)
     .init({
         resources,
         fallbackLng: 'en',
-        lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-        // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-        // if you're using a language detector, do not define the lng option
-
         interpolation: {
             escapeValue: false // react already safes from xss
+        },
+        detection: {
+            order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+            caches: ['localStorage', 'cookie']
         }
     });
 

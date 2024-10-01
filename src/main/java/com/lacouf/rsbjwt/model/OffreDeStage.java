ae470341bea93a62e6e4aa0cb9215f0a91efdedd
Loky115 <lokeshwarbasdeo23@hotmail.com>
2024-10-01 15:20:33 -0400

    classe offre de stage creer avec les variables

--- src/main/java/com/lacouf/rsbjwt/model/OffreDeStage.java
+++ src/main/java/com/lacouf/rsbjwt/model/OffreDeStage.java
@@ -1,4 +1,30 @@
 package com.lacouf.rsbjwt.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class OffreDeStage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titre;
+    private String description;
+    private String responsabilites;
+    private String qualifications;
+    private String duree;
+    private String localisation;
+    private String salaire;
+    private LocalDate dateLimite;
+
+    @ManyToOne
+    @JoinColumn(name = "employeur_id")
+    private Employeur employeur;
 }

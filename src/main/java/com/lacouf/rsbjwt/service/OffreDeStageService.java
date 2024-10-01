85fe6d84ca58de84f1d3d727a1fd4cc797a16ed9
Loky115 <lokeshwarbasdeo23@hotmail.com>
2024-10-01 15:47:25 -0400

    ÈèÈla fonction save un offre de stage creer

--- src/main/java/com/lacouf/rsbjwt/service/OffreDeStageService.java
+++ src/main/java/com/lacouf/rsbjwt/service/OffreDeStageService.java
@@ -1,7 +1,19 @@
 package com.lacouf.rsbjwt.service;
 
+import com.lacouf.rsbjwt.model.OffreDeStage;
+import com.lacouf.rsbjwt.repository.OffreDeStageRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OffreDeStageService {
+
+    private final OffreDeStageRepository offreDeStageRepository;
+
+    public OffreDeStageService(OffreDeStageRepository offreDeStageRepository) {
+        this.offreDeStageRepository = offreDeStageRepository;
+    }
+
+    public OffreDeStage creerOffreDeStage(OffreDeStage offre) {
+        return offreDeStageRepository.save(offre);
+    }
 }

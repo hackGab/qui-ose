8309e3717d453821e31f94928009941b2bc057f6
Angel Tenorio <angeltenorio3107@gmail.com>
2024-10-03 10:18:59 -0400

    Ajout tests pour le gestionnaire

---
+++ src/test/java/com/lacouf/rsbjwt/service/GestionnaireServiceTest.java
@@ -0,0 +1,68 @@
+package com.lacouf.rsbjwt.service;
+
+import com.lacouf.rsbjwt.model.Gestionnaire;
+import com.lacouf.rsbjwt.model.auth.Role;
+import com.lacouf.rsbjwt.repository.GestionnaireRepository;
+import com.lacouf.rsbjwt.service.dto.CredentialDTO;
+import com.lacouf.rsbjwt.service.dto.GestionnaireDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class GestionnaireServiceTest {
+
+    private Gestionnaire gestionnaireEntity;
+    private GestionnaireDTO gestionnaireDTO;
+
+    private GestionnaireService gestionnaireService;
+    private GestionnaireRepository gestionnaireRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        gestionnaireRepository = Mockito.mock(GestionnaireRepository.class);
+        passwordEncoder = Mockito.mock(PasswordEncoder.class);
+        gestionnaireService = new GestionnaireService(gestionnaireRepository, passwordEncoder);
+
+        gestionnaireEntity = new Gestionnaire("Thiraiyan", "Moon", "titi@gmail.com", "password", "123-456-7890");
+        gestionnaireDTO = new GestionnaireDTO(gestionnaireEntity);
+    }
+
+    @Test
+    public void testCreerGestionnaire() {
+        // Arrange
+        when(gestionnaireRepository.save(any(Gestionnaire.class)))
+                .thenReturn(gestionnaireEntity);
+
+        // Act
+        Optional<GestionnaireDTO> response = gestionnaireService.creerGestionnaire(gestionnaireDTO);
+        GestionnaireDTO gestionnaireRecu = response.get();
+
+        // Assert
+        assert response.isPresent();
+        assertEquals(gestionnaireDTO.getFirstName(), gestionnaireRecu.getFirstName());
+        assertEquals(gestionnaireDTO.getLastName(), gestionnaireRecu.getLastName());
+        assertEquals(gestionnaireDTO.getPhoneNumber(), gestionnaireRecu.getPhoneNumber());
+    }
+
+    @Test
+    public void testCreerGestionnaireWithException() {
+        // Arrange
+        when(gestionnaireRepository.save(any(Gestionnaire.class)))
+                .thenThrow(new RuntimeException());
+
+        // Act
+        Optional<GestionnaireDTO> response = gestionnaireService.creerGestionnaire(gestionnaireDTO);
+
+        // Assert
+        assert !response.isPresent();
+    }
+}

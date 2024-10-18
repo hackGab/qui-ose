846b72c4430ca0980befeff26fd7f66e1ea26e5e
Angel Tenorio <angeltenorio3107@gmail.com>
2024-10-17 23:31:39 -0400

    Header indique le nombre de stages appliqués

--- reactquiose/src/components/EtudiantHeader.js
+++ reactquiose/src/components/EtudiantHeader.js
@@ -61,7 +61,7 @@ function EtudiantHeader() {
                     </Link>
                 </div>
 
-                {file && file.status === "validé" && (
+                {file && file.status === "validé" && location.pathname === "/accueilEtudiant" && (
                     <div className="nav-text-center">
                         <a onClick={() => navigate('/stagesAppliquees', { state: { userData } })}>
                             <span>Stages Appliquées ({stagesAppliquees.length})</span>

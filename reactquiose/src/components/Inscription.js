d5487bc8f2f7fdc77e3b81251020d4ab3c54aeab
Gabriel Laplante <2215033@claurendeau.qc.ca>
2024-10-31 12:07:12 -0400

    Ajout dans security configuration et fetch pour récupérer la liste des départements

--- reactquiose/src/components/Inscription.js
+++ reactquiose/src/components/Inscription.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import InputMask from 'react-input-mask';
 import { Else, If, Then } from 'react-if';
@@ -31,28 +31,46 @@ function Inscription() {
     const [iconConf, setIconConf] = useState(eyeOff);
     const [departement, setDepartement] = useState('');
     const [selectedDepartement, setSelectedDepartement] = useState(null);
-    const optionsDepartement = [
-        { value: 'option1', label: 'Option 1' },
-        { value: 'option2', label: 'Option 2' },
-        { value: 'option3', label: 'Option 3' },
-        { value: 'option4', label: 'Option 4' },
-        { value: 'option5', label: 'Option 5' },
-        { value: 'option6', label: 'Option 6' },
-        { value: 'option7', label: 'Option 7' },
-        { value: 'option8', label: 'Option 8' },
-        { value: 'option9', label: 'Option 9' },
-        { value: 'option10', label: 'Option 10' },
-        { value: 'option11', label: 'Option 11' },
-        { value: 'option12', label: 'Option 12' },
-        { value: 'option13', label: 'Option 13' },
-        { value: 'option14', label: 'Option 14' },
-        { value: 'option15', label: 'Option 15' },
-        { value: 'option16', label: 'Option 16' }
-    ];
+    const [optionsDepartement, setOptionsDepartement] = useState([]);
+    // const optionsDepartement = [
+    //     { value: 'option1', label: 'Option 1' },
+    //     { value: 'option2', label: 'Option 2' },
+    //     { value: 'option3', label: 'Option 3' },
+    //     { value: 'option4', label: 'Option 4' },
+    //     { value: 'option5', label: 'Option 5' },
+    //     { value: 'option6', label: 'Option 6' },
+    //     { value: 'option7', label: 'Option 7' },
+    //     { value: 'option8', label: 'Option 8' },
+    //     { value: 'option9', label: 'Option 9' },
+    //     { value: 'option10', label: 'Option 10' },
+    //     { value: 'option11', label: 'Option 11' },
+    //     { value: 'option12', label: 'Option 12' },
+    //     { value: 'option13', label: 'Option 13' },
+    //     { value: 'option14', label: 'Option 14' },
+    //     { value: 'option15', label: 'Option 15' },
+    //     { value: 'option16', label: 'Option 16' }
+    // ];
 
     const [nomEntreprise, setNomEntreprise] = useState('');
     const [errorMessages, setErrorMessages] = useState('');
     const navigate = useNavigate();
+
+
+    // Get option departement
+    useEffect(() => {
+        fetch('http://localhost:8081/user/departements')
+            .then(response => response.json())
+            .then(data => {
+                const options = data.map(option => {
+                    return { value: option, label: option };
+                });
+                setOptionsDepartement(options);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des départements:', error);
+            });
+    }, []);
+
 
     const handleChangeDepartement = (option) => {
         setSelectedDepartement(option);

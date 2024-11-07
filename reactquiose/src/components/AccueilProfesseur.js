bc76737a7eb598196eed4b05c9741f871d6f183f
Angel Tenorio <angeltenorio3107@gmail.com>
2024-11-06 22:12:21 -0500

    Fonctionne

--- reactquiose/src/components/AccueilProfesseur.js
+++ reactquiose/src/components/AccueilProfesseur.js
@@ -68,35 +68,30 @@ function AccueilProfesseur() {
         fetchData();
     }, [userData]);
 
-    // useEffect(() => {
-    //     if (userData?.credentials?.email) {
-    //         console.log("userData", userData);
-    //         const fetchEvaluations = async () => {
-    //             try {
-    //                 const response = await fetch(`http://localhost:8081/professeur/evaluations/${userData.credentials.email}`);
-    //
-    //                 if (!response.ok) {
-    //                     throw new Error("Erreur lors de la récupération des évaluations");
-    //                 }
-    //
-    //                 console.log("response", response);
-    //
-    //                 // const data = await response.json();
-    //                 // console.log("data", data);
-    //                 // setListeEvaluations(data);
-    //             } catch (error) {
-    //                 console.error("Erreur lors de la récupération des données :", error);
-    //             }
-    //         };
-    //
-    //         fetchEvaluations();
-    //     }
-    // }, [userData?.credentials?.email]);
+    useEffect(() => {
+         if (userData?.credentials?.email) {
+             console.log("userData", userData);
+             const fetchEvaluations = async () => {
+                 try {
+                     const response = await fetch(`http://localhost:8081/professeur/evaluations/${userData.credentials.email}`);
 
+                     if (!response.ok) {
+                         throw new Error("Erreur lors de la récupération des évaluations");
+                     }
 
+                     console.log("response", response);
 
+                     const data = await response.json();
+                     console.log("data", data);
+                     setListeEvaluations(data);
+                 } catch (error) {
+                     console.error("Erreur lors de la récupération des données :", error);
+                 }
+             };
 
-
+             fetchEvaluations();
+         }
+     }, [userData?.credentials?.email]);
 
     const handleShowModal = (etudiant) => {
         setSelectedEtudiant(etudiant);

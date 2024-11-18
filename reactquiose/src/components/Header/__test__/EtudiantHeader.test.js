91e1b3cb4a86dbb772bbe2dc26c2307f36a7424a
Gabriel Laplante <2215033@claurendeau.qc.ca>
2024-11-17 21:29:21 -0500

    Ajout du BE et légère modification des imports dans le test EtudiantHeader

--- reactquiose/src/components/Header/__test__/EtudiantHeader.test.js
+++ reactquiose/src/components/Header/__test__/EtudiantHeader.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import EtudiantHeader, { translateTimeAgo, extractTimeAndUnit } from '../EtudiantHeader';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import i18n from '../../../utils/i18n/i18n';
+import {extractTimeAndUnit, translateTimeAgo} from "../../../utils/notificationUtils";
+import EtudiantHeader from "../EtudiantHeader";
 
 describe('EtudiantHeader Component - Notifications', () => {
     const userData = {

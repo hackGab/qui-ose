4889bb59cda75d2af74ed495d8e8b43bbef8f3eb
Loky115 <lokeshwarbasdeo23@gmail.com>
2024-12-01 22:33:34 -0500

    story 121 fait

--- reactquiose/src/components/Entrevues/MesEntrevues.js
+++ reactquiose/src/components/Entrevues/MesEntrevues.js
@@ -7,6 +7,9 @@ import EtudiantHeader from "../Header/EtudiantHeader";
 import AffichageEntrevue from "./AffichageEntrevue";
 import i18n from "i18next";
 import {getLocalStorageSession} from "../../utils/methodes/getSessionLocalStorage";
+import {FaCalendarAlt, FaMapMarkerAlt, FaSearch} from "react-icons/fa";
+import {Button} from "react-bootstrap";
+
 
 function MesEntrevues() {
     const { t } = useTranslation();
@@ -15,7 +18,10 @@ function MesEntrevues() {
     const [entrevues, setEntrevues] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [session, setSession] = useState(getLocalStorageSession())
+    const [session, setSession] = useState(getLocalStorageSession());
+    const [searchTitle, setSearchTitle] = useState('');
+    const [searchDate, setSearchDate] = useState('');
+    const [searchCompany, setSearchCompany] = useState('');
 
     const fetchSession = (session) => {
         console.log('Session:', session);
@@ -75,10 +81,17 @@ function MesEntrevues() {
         EtudiantHeader.nbEntrevuesEnAttente = EtudiantHeader.nbEntrevuesEnAttente - 1;
     };
 
+    const filterEntrevues = () => {
+        return entrevues.filter(entrevue => {
+            const titleMatch = entrevue.offreDeStageDTO.titre.toLowerCase().includes(searchTitle.toLowerCase());
+            const dateMatch = entrevue.dateHeure.includes(searchDate);
+            const companyMatch = entrevue.offreDeStageDTO.employeur.entreprise.toLowerCase().includes(searchCompany.toLowerCase());
+            return titleMatch && dateMatch && companyMatch;
+        });
+    };
 
-    const entrevuesAccepter = entrevues.filter(entrevue => entrevue.status.toLowerCase() === 'accepter');
-    const entrevuesEnAttente = entrevues.filter(entrevue => entrevue.status.toLowerCase() === 'en attente');
-
+    const entrevuesAccepter = filterEntrevues().filter(entrevue => entrevue.status.toLowerCase() === 'accepter');
+    const entrevuesEnAttente = filterEntrevues().filter(entrevue => entrevue.status.toLowerCase() === 'en attente');
 
     if (loading) {
         return <div className="text-center mt-5">
@@ -98,17 +111,65 @@ function MesEntrevues() {
         fetchSession(session.session)
     }
 
-
-
+    function clearInputs() {
+        setSearchTitle('');
+        setSearchCompany('');
+        setSearchDate('');
+    }
 
     return (
         <>
             <EtudiantHeader userData={userData} onSendData={verificationSession} />
             <div className="container-fluid p-4">
                 <div className="container flex-grow-1 pt-4">
-                    <h1 className="mb-0 text-center" style={{ fontSize: "4em" }}>{t('entrevueListTitle')}</h1>
-                    <p className="text-center mb-4" style={{ fontSize: "2em" }}>{t('entrevueListSubtitle')}</p>
-                    <small style={{ fontSize: "1rem"}}>{t('entrevueListConseil')}</small>
+                    <h1 className="mb-0 text-center" style={{fontSize: "4em"}}>{t('entrevueListTitle')}</h1>
+                    <p className="text-center mb-4" style={{fontSize: "2em"}}>{t('entrevueListSubtitle')}</p>
+                    <small style={{fontSize: "1rem"}}>{t('entrevueListConseil')}</small>
+
+                    <div className="row mt-3 mb-3">
+                        <div className="col-md-10 m-auto">
+                            <div className="input-group">
+                                <input
+                                    type="text"
+                                    className="form-control search-input"
+                                    placeholder={t('RechercherParTitre')}
+                                    value={searchTitle}
+                                    onChange={(e) => setSearchTitle(e.target.value)}
+                                />
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="search-title-icon">
+                                        <FaSearch/>
+                                    </span>
+                                </div>
+                                <div className="vertical-bar">|</div>
+                                <input
+                                    type="text"
+                                    className="form-control search-input"
+                                    placeholder={t('RechercherParEntreprise')}
+                                    value={searchCompany}
+                                    onChange={(e) => setSearchCompany(e.target.value)}
+                                />
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="search-company-icon">
+                                        <FaMapMarkerAlt/>
+                                    </span>
+                                </div>
+                                <div className="vertical-bar">|</div>
+                                <input
+                                    type="date"
+                                    className="form-control search-input"
+                                    value={searchDate}
+                                    onChange={(e) => setSearchDate(e.target.value)}
+                                />
+                                <div className="input-group-append">
+                                    <Button variant="outline-secondary" onClick={clearInputs}>
+                                        {t('Effacer')}
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="row p-2 text-center w-100 m-auto">
                         <div className="col-md-5 m-auto">
                             <h2 className="entrevuesTitreBox">{t('Acceptees')}</h2>
